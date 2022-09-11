83ee7570d0fa30e6ece09e3391b9adb9d507bba6
JR <jose.alonso.programmer@gmail.com>
2022-09-12 00:15:39 +0200

    Custom animation using Tailwind features

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export const Sidebar = () => {
 
     return (
@@ -6,7 +8,7 @@ export const Sidebar = () => {
          xl:items-start xl:w-[340px] p-2 fixed h-full">
             <div className="flex items-center justify-center w-14 h-14
             hoverAnimation p-0 xl:ml-24">
-
+                <Image src="https://rb.gy/ogau5a" width={30} height={30} />
             </div>
 
         </div>
