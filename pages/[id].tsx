f7f49eb3b749ad5ffc8276d6049a25be64743697
JR <jose.alonso.programmer@gmail.com>
2022-09-15 16:19:18 +0200

    Consider what to do when there is no session

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -1,4 +1,4 @@
-import { useSession } from "next-auth/react";
+import { getProviders, getSession, useSession } from "next-auth/react";
 import Head from 'next/head'
 import { useRecoilState } from 'recoil';
 import { modalState } from '../atoms/modelAtom';
@@ -8,8 +8,43 @@ import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { db } from "../configs/firebase";
 import { doc, DocumentData, onSnapshot } from "firebase/firestore";
+import { Login } from "../components/Login";
+import { Provider } from "next-auth/providers";
 
-export default function PostPage() {
+export async function getServerSideProps(context: any) {
+    const trendingResults = {};
+    const followResults = {};
+    // const trendingResults = { name: "dkfj", tag: "fashion" }; //= await fetch("https://jsonkeeper.com/b/NKEV").then(
+    //   (res) => res.json(),
+    // );
+    // const followResults = { influencer: "Kally Mani", country: "India" }; //= await fetch("https://jsonkeeper.com/b/WWMJ").then(
+    //   (res) => res.json(),
+    // );
+    const providers = await getProviders();
+    const session = await getSession(context);
+
+    // return providers?.google;
+    // {
+    //   trends: trendingResults,
+    //   follow: followResults,
+    // }
+    return {
+        props: {
+            trendingResults,
+            followResults,
+            providers,
+            session,
+        },
+    };
+}
+
+interface PostPageProps {
+    trendingResults: any,
+    followResults: any,
+    providers: Provider[],
+}
+
+export default function PostPage({ trendingResults, followResults, providers }: PostPageProps) {
     /*
     In next.js all pages has to be exported default. 
     Otherwise you get the error message "The default export is not a React Component in page: "/" "
@@ -27,6 +62,8 @@ export default function PostPage() {
             }),
         [db]
     );
+
+    if (!session) return <Login providers={providers} />
 
     return (
         <div className="">
@@ -48,3 +85,4 @@ export default function PostPage() {
 
 // I get this message if I don't use "default" --->
 // Error: The default export is not a React Component in page "/[id]"
+
