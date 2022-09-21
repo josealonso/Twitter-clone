008e2610f4491a5895c48c89af8211191a14c8d2
JR <jose.alonso.programmer@gmail.com>
2022-09-21 13:14:57 +0200

    Use only Google provider because it's the only one working.

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -12,19 +12,20 @@ export default NextAuth({
             // @ts-ignore
             clientSecret: process.env.GOOGLE_CLIENT_SECRET !== undefined ? process.env.GOOGLE_CLIENT_SECRET : undefined,
         }),
-        TwitterProvider({
-            // @ts-ignore
-            clientId: process.env.TWITTER_CLIENT_ID,
-            // @ts-ignore
-            clientSecret: process.env.TWITTER_CLIENT_SECRET !== undefined ? process.env.TWITTER_CLIENT_SECRET : undefined,
-        }),
-        LinkedinProvider({
-            // @ts-ignore
-            clientId: process.env.LINKEDIN_CLIENT_ID,
-            // @ts-ignore
-            clientSecret: process.env.LINKEDIN_CLIENT_SECRET !== undefined ? process.env.LINKEDIN_CLIENT_SECRET : undefined,
-            authorizationUrl: `https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=${process.env.LINKEDIN_CLIENT_ID}&redirect_uri=&state=a_random_string_that_is_really_difficult_and_random_2341344&scope=r_liteprofile%20r_emailaddress`,
-        }),
+        // TwitterProvider({
+        //     // @ts-ignore
+        //     clientId: process.env.TWITTER_CLIENT_ID,
+        //     // @ts-ignore
+        //     clientSecret: process.env.TWITTER_CLIENT_SECRET !== undefined ? process.env.TWITTER_CLIENT_SECRET : undefined,
+        // }),
+        // LinkedinProvider({
+        //     // @ts-ignore
+        //     clientId: process.env.LINKEDIN_CLIENT_ID,
+        //     // @ts-ignore
+        //     clientSecret: process.env.LINKEDIN_CLIENT_SECRET !== undefined ? process.env.LINKEDIN_CLIENT_SECRET : undefined,
+        //     authorizationUrl: `https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=${process.env.LINKEDIN_CLIENT_ID}&redirect_uri=${process.env.CALLBACK_URL}&state=a_random_string_that_is_really_difficult_and_random_2341344&scope=r_liteprofile%20r_emailaddress`,
+        //     // https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id={your_client_id}&redirect_uri={your_callback_url}&state=foobar&scope=r_liteprofile%20r_emailaddress
+        // }),
 
         // add more providers here
     ],
